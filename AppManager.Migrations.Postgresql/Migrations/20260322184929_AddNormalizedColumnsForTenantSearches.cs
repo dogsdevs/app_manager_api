42a7df626293396dade90449b5bf39ecbc7802a2
jorgeurquijo <jorgeurqdzo@gmail.com>
2026-03-22 11:52:44 -0700

    Add migrations for tenants normalized column

---
+++ AppManager.Migrations.Postgresql/Migrations/20260322184929_AddNormalizedColumnsForTenantSearches.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AppManager.Migrations.Postgresql.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNormalizedColumnsForTenantSearches : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "NormalizedNameAndSlug",
+                schema: "admin",
+                table: "Tenants",
+                type: "text",
+                nullable: false,
+                computedColumnSql: "upper(coalesce(\"Name\", '') || ' ' || coalesce(\"Slug\", ''))",
+                stored: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tenants_NormalizedNameAndSlug",
+                schema: "admin",
+                table: "Tenants",
+                column: "NormalizedNameAndSlug");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Tenants_NormalizedNameAndSlug",
+                schema: "admin",
+                table: "Tenants");
+
+            migrationBuilder.DropColumn(
+                name: "NormalizedNameAndSlug",
+                schema: "admin",
+                table: "Tenants");
+        }
+    }
+}
